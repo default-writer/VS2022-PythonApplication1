ec0bc006ee430a89861a2286b78fbb156ece388f
funcelot <funcelot@outlook.com>
2021-12-04 21:19:40 +0300

    fix naming

---
+++ tests/test_default.py
@@ -0,0 +1,12 @@
+# pylint:disable=unnecessary-pass
+""" Tests """
+import unittest
+
+class TestDefault(unittest.TestCase):
+    """ Default methods tests """
+    def test_method(self):
+        """ Method test """
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
