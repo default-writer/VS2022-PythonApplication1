1c6e69021333ea7d3724029e3eebc916c73a4d77
funcelot <funcelot@outlook.com>
2021-12-04 18:17:00 +0000

    added defaut docstrings

--- tests/test_Sales.py
+++ tests/test_Sales.py
@@ -1,7 +1,11 @@
+# pylint:disable=unnecessary-pass
+""" Tests """
 import unittest
 
-class Test_Sales(unittest.TestCase):
-    def test_A(self):
+class TestSales(unittest.TestCase):
+    """ Sales tests """
+    def test_a(self):
+        """ a test """
         pass
 
 if __name__ == '__main__':
