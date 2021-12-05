d0f483a1f486c3cb713b141c35b82414dd9b964a
funcelot <funcelot@outlook.com>
2021-12-05 05:07:38 +0000

    added pwsh and some steroids for fun

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-cd "${0%/*}"
+cd "${0%/*}/.."
 
-pip3 install -r requirements.txt
+sudo apt-get update && apt-get upgrade -y && apt-get install virtualenv -y
+
+virtualenv venv
+
+. venv/bin/activate
+
+python3 -m pip install --upgrade pip
+
+pip install -r $PWD/bin/build_requirements.txt #  1> /dev/null
