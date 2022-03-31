591f8850e9ef79d6aa11d2c48253baa6185a4891
Artur Mustafin <funcelot@outlook.com>
2022-03-31 04:35:02 +0000

    fixed dotnet script install

--- install_dotnet_script.sh
+++ install_dotnet_script.sh
@@ -10,24 +10,16 @@ set -e
 # Shell
 #wget https://dot.net/v1/dotnet-install.sh && chmod +x ./dotnet-install.sh && sudo ./dotnet-install.sh
 
-export PATH="$PATH:$HOME/.dotnet"
 export DOTNET_ROOT="$HOME/.dotnet"
 export PATH="$PATH:$DOTNET_ROOT"
-
-[ -f $DOTNET_ROOT/tools/dotnet-script ] && dotnet tool uninstall dotnet-script -g || echo ERROR
-dotnet tool install dotnet-script -g
-
 export PATH="$PATH:$DOTNET_ROOT/tools"
 
-cat << \EOF > ~/.bash_profile
-export PATH=$PATH:$HOME/.dotnet
+[ -f $DOTNET_ROOT/tools/dotnet-script ] && dotnet tool uninstall dotnet-script -g
+dotnet tool install dotnet-script -g
+
+cat << \EOF >> ~/.basrc
 export DOTNET_ROOT=$HOME/.dotnet
 export PATH=$PATH:$DOTNET_ROOT
-EOF
-
-cat << \EOF >> ~/.bash_profile
 # Add .NET Core SDK tools
 export PATH="$PATH:$DOTNET_ROOT/tools"
 EOF
-
-source ~/.bash_profile
